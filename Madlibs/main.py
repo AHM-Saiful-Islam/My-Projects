7097e1223c1e6204ebef627d1e7f214ec055220b
A H M SAIFUL ISLAM <iahmsaiful@gmail.com>
2023-11-26 04:27:18 +0600

    madlibs update

---
+++ Madlibs/main.py
@@ -0,0 +1,19 @@
+# # string concatenation
+# youtuber = "Saiful Islam"
+
+# # ways
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}") # abstring method
+
+adj = input("Adjective: ")
+verb1 = input("verb1: ")
+verb2 = input("verb2: ")
+famous_person = input("Famous_Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me \
+so exited all the time because I love to {verb1}. \
+Stay hydrated and {verb2} like you are \
+{famous_person}!"
+
+print(madlib)
